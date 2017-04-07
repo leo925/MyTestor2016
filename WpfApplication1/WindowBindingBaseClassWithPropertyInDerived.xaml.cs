b19d63080c1073556dd67b1dd8314aa9ba52c712
ACTIVE\zliu <zhihang.liu@activenetwork.com>
2017-04-07 15:29:52 +0800

    add 7000

---
+++ WpfApplication1/WindowBindingBaseClassWithPropertyInDerived.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for WindowBindingBaseClassWithPropertyInDerived.xaml
+    /// </summary>
+    public partial class WindowBindingBaseClassWithPropertyInDerived : Window, INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public WindowBindingBaseClassWithPropertyInDerived()
+        {
+            InitializeComponent();
+
+
+            this.DataContext = this;
+        }
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            this.VM = new DerivedTestClass()
+            {
+                Name = "name1111",
+                Name2 = "name22222"
+            };
+        }
+        private BaseTestViewModel vm;
+
+
+        public BaseTestViewModel VM
+        {
+            get
+            {
+                return vm;
+            }
+            set
+            {
+                vm = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("VM"));
+                }
+            }
+        }
+    }
+
+    public class BaseTestViewModel : BaseViewModel
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                base.OnPropertyChanged("Name");
+            }
+        }
+
+    }
+
+    public class DerivedTestClass : BaseTestViewModel
+    {
+
+        private string name2;
+
+        public string Name2
+        {
+            get
+            {
+                return name2;
+            }
+            set
+            {
+                name2 = value;
+                base.OnPropertyChanged("Name");
+            }
+        }
+
+    }
+
+
+}
---
+++ WpfApplication1/WindowCursor.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for WindowCursor.xaml
+    /// </summary>
+    public partial class WindowCursor : Window
+    {
+        public WindowCursor()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ComboBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ComboBox a;
+
+        }
+
+        private void ComboBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    public class ImageGifView : MediaElement
+    {
+        public ImageGifView()
+        {
+            this.Loaded += ImageGifView_Loaded;
+        }
+
+        private string imgSource;
+
+        public string ImgSource
+        {
+            get
+            {
+                return imgSource;
+            }
+            set
+            {
+                imgSource = value;
+            }
+        }
+
+        private void ImageGifView_Loaded(object sender, RoutedEventArgs e)
+        {
+            string str = @"E:\git\MyTestor2016\WpfApplication1\pic\battery_unknown5.gif";
+            
+            
+            this.Width = 24;
+            this.Height = 24;
+            this.LoadedBehavior = MediaState.Play;
+            this.Stretch = Stretch.Fill;
+
+            //循环播放
+            if (true)
+            {
+                this.MediaEnded += ImageGifView_MediaEnded;
+            }
+
+        }
+
+        /// <summary>
+        /// 结束后事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ImageGifView_MediaEnded(object sender, RoutedEventArgs e)
+        {
+
+            MediaElement media = (MediaElement)sender;
+            media.Position = TimeSpan.FromMilliseconds(1);
+            //media.Play();
+
+        }
+    }
+}
